40f6794e3076a80cff89c874d4c7ff2383fc9e48
Gavind <17674755974@163.com>
2019-09-09 23:53:26 +0800

    小刷

---
+++ src/com/usc/leetcode/Zconvert.java
@@ -0,0 +1,21 @@
+package com.usc.leetcode;
+
+public class Zconvert{
+    public static void main(String[] args) {
+        
+    }
+
+
+
+    //TODO:继续刷题
+    public String convert(String s, int numRows) {
+        StringBuilder []sbs = new StringBuilder[numRows];
+        char[] chs = s.toCharArray();
+        for(int i = 0, j = 0; i < numRows; i++){
+            //sbs[j] = chs[]
+        }
+
+
+        return "";
+    }
+}
