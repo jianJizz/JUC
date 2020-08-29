25aaf016ba2489db42995892c9a5784c6beac62b
Gavind <17674755974@163.com>
2020-08-29 18:02:12 +0800

    test

--- src/com/usc/javase/EnumTest.java
+++ src/com/usc/javase/EnumTest.java
@@ -7,5 +7,18 @@ package com.usc.javase;
 public class EnumTest {
     public static void main(String[] args) {
         System.out.println(Season.SPRING);
+        Season season = Season.SPRING;
+        switch (season){
+            case SPRING:
+                System.out.println("spring, code is: " + season.getCode());
+                break;
+            case SUMMER:
+                System.out.println("summer, code is: " + season.getCode());
+                break;
+            default:
+                System.out.println("No this Season");
+                break;
+        }
+
     }
 }
--- src/com/usc/javase/Season.java
+++ src/com/usc/javase/Season.java
@@ -5,5 +5,14 @@ package com.usc.javase;
  * @date 2020/8/29 3:14 下午
  */
 public enum Season {
-    SPRING, SUMMER, AUTUMN, WINTER
+    SPRING(1), SUMMER(2), AUTUMN(3), WINTER(4);
+
+    private int code;
+    private Season(int code){
+        this.code = code;
+    }
+
+    public int getCode(){
+        return this.code;
+    }
 }
